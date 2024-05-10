95cb18396e08903108578695b6e5a56cc8146a61
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-10 23:15:10 +0200

    Added top navigation HTML elements

--- index.php
+++ index.php
@@ -1,14 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Reset CSS -->
   <link rel="stylesheet" href="./assets/css/reset.css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/bootstrap/icons/font/bootstrap-icons.min.css">
+  <!-- Custom CSS -->
   <link rel="stylesheet" href="./assets/css/style.css">
+  <!-- Tab title -->
   <title>Welkom | eduBridge</title>
 </head>
 <body>
-  <h1>Welkom bij eduBridge!</h1>
+  <!-- Start header content -->
+  <header>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top">
+      <div class="container">
+        <!-- Language switcher container -->
+        <div class="d-flex w-100 align-items-center justify-content-between border-bottom pb-2 mb-5">
+          <ul class="d-flex gap-2 p-0 mb-0">
+            <li><a href="#">EN</a></li>
+            <li><a href="#">FR</a></li>
+            <li><a href="#">NL</a></li>
+            <li><a href="#">DE</a></li>
+          </ul>
+          <span>Official information and services: www.belgium.be</span>
+        </div>
+
+        <!-- Navigation container -->
+        <div class="d-flex w-100 justify-content-between">
+          <img src="" alt="Logo">
+          <ul class="d-flex gap-2 mb-0">
+            <li><a href="">Over</a></li>
+            <li><a href="">Nieuws</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Start main content -->
+  <main>
+
+  </main>
+
+  <!-- Start footer content -->
+  <footer>
+
+  </footer>
 
   <!-- Links JS scripts -->
   <script scr="./assets/js/app.js"></script>

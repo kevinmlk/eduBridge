d8c4c8329e50370b3364ab6327b6220f4595c8fa
Kevin Malekera <r0900654@student.thomasmore.be>
2024-05-10 22:22:51 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Welkom | eduBridge</title>
+</head>
+<body>
+  <h1>Welkom bij eduBridge!</h1>
+
+  <!-- Links JS scripts -->
+  <script scr="./assets/js/app.js"></script>
+</body>
+</html>

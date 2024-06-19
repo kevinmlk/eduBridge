be1ed0a9ba48f825ee99121da90b0dd19e292f0a
Kevin Malekera <r0900654@student.thomasmore.be>
2024-06-19 15:57:38 +0200

    Modified the dashboard nav and added active link styling

--- dashboard/roadmap.php
+++ dashboard/roadmap.php
@@ -22,35 +22,34 @@
   <title>Welkom bij eduBridge | eduBridge Belgium</title>
 </head>
 <body>
-  <!-- Start header content -->
-  <header class="mb-5">
+   <!-- Start header content -->
+  <header class="mb-4">
     <!-- Navbar -->
     <nav class="navbar">
       <div class="container">
         <!-- Language switcher container -->
-        <div class="d-flex w-100 align-items-center justify-content-between border-bottom pb-3 mb-5 mt-2">
-          <ul id="language-switcher" class="d-flex gap-2 p-0 mb-0">
+        <div class="d-flex w-100 align-items-center justify-content-between border-bottom pb-3 mb-4 mt-2">
+          <ul class="language-switcher d-flex gap-2 p-0 mb-0">
             <li><a href="#">EN</a></li>
             <li><a href="#">FR</a></li>
             <li><a href="#" class="active-lang">NL</a></li>
             <li><a href="#">DE</a></li>
           </ul>
           <div id="notification-container">
-            <a href=""><i class="bi bi-bell me-2"></i>Notificaties(3)</a>
-            <a href="./includes/logout.inc.php"><i class="bi bi-box-arrow-right me-2"></i>Afmelden</a>
+            <a href="#"><i class="bi bi-bell me-2"></i>Notificaties (3)</a>
+            <a href="./../includes/logout.inc.php"><i class="bi bi-box-arrow-right me-2"></i>Afmelden</a>
           </div>
         </div>
 
         <!-- Navigation container -->
         <div class="d-flex w-100 justify-content-between align-items-center">
-          <a href="index.php" class="navbar-brand"><img class="me-2" src="./assets/images/logo/edubridge-logo.svg" alt="eduBridge logo">eduBridge</a>
+          <a href="index.php" class="navbar-brand"><img class="me-2" src="./../assets/images/logo/edubridge-logo.svg" alt="eduBridge logo">eduBridge</a>
             <ul class="nav d-flex gap-2">
-              <li class="nav-item"><a href="./about.php" aria-page="current" class="nav-link active">Roadmap</a></li>
-              <li class="nav-item"><a href="./news.php" class="nav-link">Simulator</a></li>
-              <li class="nav-item"><a href="./faq.php" class="nav-link">Procedures</a></li>
-              <li class="nav-item"><a href="./contact.php" class="nav-link">Info hub</a></li>
-              <li class="nav-item"><a href="./contact.php" class="nav-link">Profiel</a></li>
-              <li class="nav-item ms-3"><a href="./login.php" class="btn btn-primary"><i class="bi bi-person-circle me-2"></i>Kinderprofiel</a></li>
+              <li class="nav-item"><a href="#" aria-page="current" class="nav-link active">Roadmap</a></li>
+              <li class="nav-item"><a href="./simulator.php" class="nav-link">Simulator</a></li>
+              <li class="nav-item"><a href="./procedures.php" class="nav-link">Procedures</a></li>
+              <li class="nav-item"><a href="./info-hub.php" class="nav-link">Info hub</a></li>
+              <li class="nav-item"><a href="./profile.php" class="nav-link">Profiel</a></li>
             </ul>
         </div>
       </div>

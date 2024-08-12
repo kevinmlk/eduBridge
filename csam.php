e8dd8ebd9137df26d5321b433cab3997a4a576b2
Kevin Malekera <r0900654@student.thomasmore.be>
2024-08-12 21:40:37 +0200

    Modified the login page

---
+++ csam.php
@@ -0,0 +1,90 @@
+<?php
+  // Check if user is already logged in
+  session_start();
+
+  if (isset($_SESSION['loggedin'])) {
+    header('Location: ./dashboard/roadmap.php');
+    exit;
+  }
+?><!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Reset CSS -->
+  <link rel="stylesheet" href="./assets/css/reset.css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/bootstrap/icons/font/bootstrap-icons.min.css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <!-- Tab title -->
+  <title>Authenticatie | eduBridge Belgium</title>
+</head>
+<body>
+  <!-- Start header content -->
+  <header class="mb-4">
+    <!-- Language switcher container -->
+    <div class="container d-flex w-100 align-items-center justify-content-between border-bottom pb-3 mt-3">
+      <ul class="language-switcher d-flex gap-2 p-0 mb-0">
+        <li><a href="#">EN</a></li>
+        <li><a href="#">FR</a></li>
+        <li><a href="#" class="active-lang">NL</a></li>
+        <li><a href="#">DE</a></li>
+      </ul>
+      <p class="fs-6">
+        Officiële informatie en diensten: www.belgium.be
+      </p>
+    </div>
+
+    <div class="theme-purple p-4">
+      <!-- Navigation container -->
+      <div id="navbar-alt-container" class="container">
+        <a href="index.php" class="d-flex align-items-center"><img class="me-3 navbar-brand-alt" src="./assets/images/logo/edubridge-logo-white.svg" alt="eduBridge logo">www.eduBridge.be</a>
+      </div>
+    </div>
+  </header>
+
+  <!-- Start main -->
+  <main class="d-flex flex-column gap-5 mb-5 pb-5">
+      <!-- Authentication section -->
+     <section class="container d-flex flex-column gap-4 mt-5 mb-5">
+      <div class="d-flex flex-column gap-4 mb-4">
+        <div>
+          <h2>Aanmelden</h2>
+          <span class="tag-line theme-blue"></span>
+        </div>
+        <p>Kies hieronder hoe u wil aanmelden. Klik op "meer info" voor uitleg over  die manier van aanmelden.</p>
+        <p>Klik op de knop "hulp nodig?" (rechts) voor  veelgestelde vragen over aanmelden of om contact op te nemen met de  helpdesk.</p>
+      </div>
+
+      <div class="d-flex flex-wrap gap-5">
+        <div class="login-card col-4 d-flex flex-column gap-4">
+          <a href="./includes/authenticate.inc.php" class="d-flex align-items-top gap-4">
+            <i class="bi bi-chevron-right"></i>
+            <h5>eID en aangesloten kaartlezer</h5>
+            <img class="justify-self-end" src="./assets/images/icons/id-auth.png" alt="Kaartlezer">
+          </a>
+          <span class="login-badge theme-green px-3 py-2 rounded ms-5">Veiligste keuze</span>
+          <a href="#" class="btn btn-link d-flex align-self-end">Meer info</a>
+        </div>
+
+        <div class="login-card col-4 d-flex flex-column gap-4">
+          <a href="./includes/auth-itsme.inc.php" class="d-flex justify-content-between align-items-top gap-4 mb-3">
+            <i class="bi bi-chevron-right"></i>
+            <h5 class="align-self-start justify-self-start">itsme®</h5>
+            <img class="justify-self-end" src="./assets/images/icons/itsme-auth.png" alt="Kaartlezer">
+          </a>
+          <a href="#" class="btn btn-link d-flex align-self-end mt-5">Meer info</a>
+        </div>
+      </div>
+     </section>
+  </main>
+
+  <!-- Start footer -->
+  <?php include_once(__DIR__ . '/includes/footer.inc.php'); ?>
+
+  <!-- Links JS scripts -->
+  <script src="./assets/js/app.js"></script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -19,64 +19,82 @@
   <!-- Custom CSS -->
   <link rel="stylesheet" href="./assets/css/style.css">
   <!-- Tab title -->
-  <title>Authenticatie | eduBridge Belgium</title>
+  <title>Aanmelden | eduBridge Belgium</title>
 </head>
 <body>
   <!-- Start header content -->
   <header class="mb-4">
-    <!-- Language switcher container -->
-    <div class="container d-flex w-100 align-items-center justify-content-between border-bottom pb-3 mt-3">
-      <ul class="language-switcher d-flex gap-2 p-0 mb-0">
-        <li><a href="#">EN</a></li>
-        <li><a href="#">FR</a></li>
-        <li><a href="#" class="active-lang">NL</a></li>
-        <li><a href="#">DE</a></li>
-      </ul>
-      <p class="fs-6">
-        Officiële informatie en diensten: www.belgium.be
-      </p>
-    </div>
+    <!-- Navbar -->
+    <nav class="navbar">
+      <div class="container">
+        <!-- Language switcher container -->
+        <div class="d-flex w-100 align-items-center justify-content-between border-bottom pb-3 mb-4 mt-2">
+          <ul class="language-switcher d-flex gap-2 p-0 mb-0">
+            <li><a href="#">EN</a></li>
+            <li><a href="#">FR</a></li>
+            <li><a href="#" class="active-lang">NL</a></li>
+            <li><a href="#">DE</a></li>
+          </ul>
+          <p id="belgium-link" class="fs-6">
+            Officiële informatie en diensten: <a href="https://www.belgium.be" target="_blank">www.belgium.be</a>
+          </p>
+        </div>
 
-    <div class="theme-purple p-4">
-      <!-- Navigation container -->
-      <div id="navbar-alt-container" class="container">
-        <a href="index.php" class="d-flex align-items-center"><img class="me-3 navbar-brand-alt" src="./assets/images/logo/edubridge-logo-white.svg" alt="eduBridge logo">www.eduBridge.be</a>
+        <!-- Navigation container -->
+        <div class="d-flex w-100 justify-content-between align-items-center">
+          <a href="index.php" class="navbar-brand"><img class="me-2" src="./assets/images/logo/edubridge-logo.svg" alt="eduBridge logo">eduBridge</a>
+        </div>
       </div>
-    </div>
+    </nav>
   </header>
 
   <!-- Start main -->
   <main class="d-flex flex-column gap-5 mb-5 pb-5">
-      <!-- Authentication section -->
-     <section class="container d-flex flex-column gap-4 mt-5 mb-5">
-      <div class="d-flex flex-column gap-4 mb-4">
+
+    <!-- Intro section -->
+     <section class="container mt-5 mb-5">
+      <div>
         <div>
-          <h2>Aanmelden</h2>
+          <h2>Welkom terug</h2>
           <span class="tag-line theme-blue"></span>
+          <p class="mt-4">
+            Vul de inputvelden in met uw gegevens om aan te melden.
+          </p>
         </div>
-        <p>Kies hieronder hoe u wil aanmelden. Klik op "meer info" voor uitleg over  die manier van aanmelden.</p>
-        <p>Klik op de knop "hulp nodig?" (rechts) voor  veelgestelde vragen over aanmelden of om contact op te nemen met de  helpdesk.</p>
+        <form class="mt-5">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Wachtwoord</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn theme-green">Aanmelden</button>
+        </form>
       </div>
-
-      <div class="d-flex flex-wrap gap-5">
-        <div class="login-card col-4 d-flex flex-column gap-4">
-          <a href="./includes/authenticate.inc.php" class="d-flex align-items-top gap-4">
-            <i class="bi bi-chevron-right"></i>
-            <h5>eID en aangesloten kaartlezer</h5>
-            <img class="justify-self-end" src="./assets/images/icons/id-auth.png" alt="Kaartlezer">
-          </a>
-          <span class="login-badge theme-green px-3 py-2 rounded ms-5">Veiligste keuze</span>
-          <a href="#" class="btn btn-link d-flex align-self-end">Meer info</a>
+      
+      <!-- Carousel -->
+      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./assets/images/pillar-image-1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./assets/images/pillar-image-2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./assets/images/pillar-image-3.jpg" class="d-block w-100" alt="...">
+          </div>
         </div>
-
-        <div class="login-card col-4 d-flex flex-column gap-4">
-          <a href="./includes/auth-itsme.inc.php" class="d-flex justify-content-between align-items-top gap-4 mb-3">
-            <i class="bi bi-chevron-right"></i>
-            <h5 class="align-self-start justify-self-start">itsme®</h5>
-            <img class="justify-self-end" src="./assets/images/icons/itsme-auth.png" alt="Kaartlezer">
-          </a>
-          <a href="#" class="btn btn-link d-flex align-self-end mt-5">Meer info</a>
-        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
      </section>
   </main>
@@ -86,5 +104,8 @@
 
   <!-- Links JS scripts -->
   <script src="./assets/js/app.js"></script>
+  <!-- Bootstrap -->
+   <scripts src="./assets/bootstrap/js/bootstrap.min.js"></scripts>
+   <scripts src="./assets/bootstrap/js/bootstrap.bundle.min.js"></scripts>
 </body>
 </html>
